0edb0cc71f5c5e2aebf74c62719625aa37352c4e
Dipam Sen <dipamdiptam@gmail.com>
2023-01-16 03:25:59 +0530

    remove error handling

--- api/index.js
+++ api/index.js
@@ -33,57 +33,52 @@ app.post(
         },
       });
 
-      try {
-        let subject;
-        if (cmd.name === "random") {
-          subject = ["physics", "chemistry", "maths"][
-            Math.floor(Math.random() * 3)
-          ];
-        } else {
-          subject = cmd.name;
-        }
-        let chapter;
-        if (cmd.options && cmd.options[0] && cmd.options[0].value) {
-          chapter = cmd.options[0].value;
-        } else {
-          const allChaps = (await getCompletedChaps())[subject];
-          chapter = allChaps[Math.floor(Math.random() * allChaps.length)];
-        }
-        const chapterInfo = await getChapters(
-          Constants.CLASS_XI,
-          Constants[subject.toUpperCase()]
-        );
-
-        const questions = await getAllQuestions([chapter]);
-        const questionMD =
-          questions.questions[
-            Math.floor(Math.random() * questions.questions.length)
-          ];
-        const qid = questionMD.QuestionId;
-        const question = await getQuestion(qid);
-
-        const subjectCap = subject.charAt(0).toUpperCase() + subject.slice(1);
-        const payload = {
-          content: `\`${qid}\``,
-          embeds: [
-            {
-              title: `Question of the Day`,
-              description: `**Topic:** ${subjectCap} - ${
-                chapterInfo.find((c) => c.courseChapterId === chapter)
-                  .chapterName
-              }
-Answer: ||${question.questionData.answerOption}||`,
-              image: {
-                url: question.questionData.questionDiagramURL,
-              },
-            },
-          ],
-        };
-        const url = `https://discord.com/api/webhooks/${body.application_id}/${token}/messages/@original`;
-        await axios.patch(url, payload);
-      } catch (e) {
-        console.error(e);
+      let subject;
+      if (cmd.name === "random") {
+        subject = ["physics", "chemistry", "maths"][
+          Math.floor(Math.random() * 3)
+        ];
+      } else {
+        subject = cmd.name;
       }
+      let chapter;
+      if (cmd.options && cmd.options[0] && cmd.options[0].value) {
+        chapter = cmd.options[0].value;
+      } else {
+        const allChaps = (await getCompletedChaps())[subject];
+        chapter = allChaps[Math.floor(Math.random() * allChaps.length)];
+      }
+      const chapterInfo = await getChapters(
+        Constants.CLASS_XI,
+        Constants[subject.toUpperCase()]
+      );
+
+      const questions = await getAllQuestions([chapter]);
+      const questionMD =
+        questions.questions[
+          Math.floor(Math.random() * questions.questions.length)
+        ];
+      const qid = questionMD.QuestionId;
+      const question = await getQuestion(qid);
+
+      const subjectCap = subject.charAt(0).toUpperCase() + subject.slice(1);
+      const payload = {
+        content: `\`${qid}\``,
+        embeds: [
+          {
+            title: `Question of the Day`,
+            description: `**Topic:** ${subjectCap} - ${
+              chapterInfo.find((c) => c.courseChapterId === chapter).chapterName
+            }
+Answer: ||${question.questionData.answerOption}||`,
+            image: {
+              url: question.questionData.questionDiagramURL,
+            },
+          },
+        ],
+      };
+      const url = `https://discord.com/api/webhooks/${body.application_id}/${token}/messages/@original`;
+      await axios.patch(url, payload);
     } else {
       res.json({
         type: 4,
